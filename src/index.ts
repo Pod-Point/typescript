0709957ce4f5ca5ce70de739d1a1b169fc6b8c63
Lee Brooks <leebrooks93@gmail.com>
2019-09-23 13:50:38 +0100

    Add to index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
+export { default as CacheService } from './cache/Service';
 export { default as Factory } from './factories/Factory';
 export { default as PayloadFactory } from './factories/PayloadFactory';
 export { default as Repository } from './http/Repository';
 export { default as Service } from './http/Service';
 export { default as Client } from './http/Client';
+export { default as LogService } from './logs/Service';
 export { default as Model } from './models/Model';
 export { default as Notification } from './notifications/Notification';
 export { default as PayloadTransformer } from './transformers/PayloadTransformer';
-export { default as LogService } from './logs/Service';
 
+export { default as CacheServiceInterface } from './types/cache/Service';
 export { default as FactoryInterface } from './types/factories/Factory';
 export { default as ClientInterface } from './types/http/Client';
 export { default as ResponseInterface } from './types/http/Response';
