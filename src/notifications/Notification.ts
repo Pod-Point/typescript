71b3038efbe15b8f2429675072a303b8a8165a59
Rick Schubert <rick.schubert@pod-point.com>
2023-07-03 16:18:54 +0100

    feat(PAR-214): Add support to send SNS Message Attributes (#15)
    This adds support for SNS message attributes. I need this for [PAR-214](https://podpoint.atlassian.net/browse/PAR-214) where I will have to modify the repository [occp-lambda](https://github.com/Pod-Point/ocpp-lambda) which uses this package to publish SNS notifications.
    
    I had to upgrade the nodejs version and dependencies due to multiple issues:
    
    - nodejs 16 because otherwise `npm run build` didn't run (I can't go higher as Travis CI literally can't install 18 or 20, it failed multiple times doing so, ridiculous)
    - Upgrade typescript version to get around build issues where dependencies were already a lot of versions ahead and using new features, therefore causing issues
    - Removed `tslint-eslint-rules` dependency as it is not compatible anymore with the new typescript version, but we need that
    - Upgraded `tslint` as it raised a warning that it didn't support that newer typescript version
    - Updates `jest` and `ts-jest` as these otherwise struggled with node version 16

--- src/notifications/Notification.ts
+++ src/notifications/Notification.ts
@@ -6,11 +6,18 @@ export default class Notification implements NotificationInterface<SNS.Types.Pub
     public topicARN: string;
     public config: SNS.ClientConfiguration = {};
     public id?: string;
+    public messageAttributes: SNS.MessageAttributeMap = {};
 
-    public constructor(message: string, topicARN: string, config?: SNS.ClientConfiguration) {
+    public constructor(
+        message: string,
+        topicARN: string,
+        config?: SNS.ClientConfiguration,
+        messageAttributes?: SNS.MessageAttributeMap,
+    ) {
         this.message = message;
         this.topicARN = topicARN;
         this.config = config;
+        this.messageAttributes = messageAttributes;
     }
 
     /**
@@ -19,6 +26,7 @@ export default class Notification implements NotificationInterface<SNS.Types.Pub
     public async dispatch(): Promise<SNS.Types.PublishResponse> {
         const message: SNS.Types.PublishInput = {
             Message: this.message,
+            MessageAttributes: this.messageAttributes,
             TopicArn: this.topicARN,
         };
 
